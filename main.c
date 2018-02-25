56f9d599e276724055621de57c158910a2179959
Hturi <ghaniturismo@gmail.com>
2018-02-25 17:38:26 +0100

    number generate

--- main.c
+++ main.c
@@ -3,33 +3,43 @@
 #include <stdlib.h>
 #include <assert.h>
 
-void fact(int n){
-  int i;
-  mpz_t p;
+gmp_randstate_t generator;
 
-  mpz_init_set_ui(p,1); /* p = 1 */
-  for (i=1; i <= n ; ++i){
-    mpz_mul_ui(p,p,i); /* p = p * i */
-  }
-  printf ("%d!  =  ", n);
-  mpz_out_str(stdout,10,p);
-  mpz_clear(p);
+//generate a number of exactly k_bit
+void generate_number(mpz_t alea, unsigned int nbr_bit)
+{
+    mpz_t k;
+    mpz_init(k);
+    mpz_ui_pow_ui(k,2,nbr_bit-1);
+    mpz_urandomb(alea, generator, nbr_bit-1);
+    mpz_add(alea,alea,k);
+    mpz_clear(k);
+}
 
+void generate_prime_number(mpz_t alea, unsigned int nbr_bit)
+{
+    int prime;
+    mpz_t k;
+    mpz_init(k);
+    mpz_ui_pow_ui(k,2,nbr_bit-1);
+    do
+    {
+        mpz_urandomb(alea, generator, nbr_bit-1);
+        mpz_add(alea,alea,k);
+        prime = mpz_probab_prime_p(alea,10);
+    }while(prime==0);
+    mpz_clear(k);
 }
 
 int main(int argc, char * argv[]){
-  int n;
+  	int n;
+	gmp_randinit_default(generator);    
+//msg to encrypt
+    mpz_t msg1,msg2;
+    mpz_inits(msg1,msg2, NULL);
+    generate_number(msg1,16);
+    generate_prime_number(msg2,16);
+    gmp_printf("msg number :%Zu \n",msg1);
+    gmp_printf("msg alea number :%Zu \n",msg2);
 
-
-  if (argc <= 1){
-    printf ("Usage: %s <number> \n", argv[0]);
-    return 2;
-  }
-
-  n = atoi(argv[1]);
-  assert( n >= 0);
-  fact(n);
-
-
-  return 1;
 }
