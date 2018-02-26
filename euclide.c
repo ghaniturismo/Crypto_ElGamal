366492385059bf49dde1410c9988c32783735439
samybtn <samybentounes@gmail.com>
2018-02-26 14:29:38 +0100

    [ADD] comments on euclide.c

--- euclide.c
+++ euclide.c
@@ -5,33 +5,20 @@
 
 /*---------------------------------------------------------------------*/
 
-
+//Declaration des variables globales
+//Variable representant la derniere valeur de u
 mpz_t uFinal;
+//Variable representant la derniere valeur de v
 mpz_t vFinal;
+//Variable reprensentant p
 mpz_t p_global;
-
-/*
-  Affiche au + bv = p
- */
-
-void printdivision(mpz_t a, mpz_t b, mpz_t q, mpz_t r) 
-{
-      mpz_out_str(NULL, 10, a);
-      printf(" = ");
-      mpz_out_str(NULL, 10, b);
-      printf(" * ");
-      mpz_out_str(NULL, 10, q);
-      printf(" + ");
-      mpz_out_str(NULL, 10, r);
-      printf("\n");
-}
 
 /*---------------------------------------------------------------------*/
 
-/*
-  Affiche au + bv = p
- */
 
+
+
+//Affichage de "a*u + b*v = p"
 void printauplusbv(mpz_t a, mpz_t u, mpz_t b, mpz_t v, mpz_t p) 
 {
       mpz_out_str(NULL, 10, a);
@@ -46,14 +33,9 @@ void printauplusbv(mpz_t a, mpz_t u, mpz_t b, mpz_t v, mpz_t p)
       printf("\n");
 }
 
-/*---------------------------------------------------------------------*/
-
-/*
-  Affiche b * uRec + (a - b*q) vRec = p
- */
-
+//Affichage de "b*uRec + (a - b*q) *vRec = p"
 void printbuplusamoinsbqv(mpz_t b, mpz_t uRec, mpz_t a, 
-			  mpz_t q, mpz_t vRec, mpz_t p)
+        mpz_t q, mpz_t vRec, mpz_t p)
 {
   printf("---------------------------------------------\n");
       mpz_out_str(NULL, 10, b);
@@ -72,13 +54,21 @@ void printbuplusamoinsbqv(mpz_t b, mpz_t uRec, mpz_t a,
       printf("\n");
 }
 
-/*---------------------------------------------------------------------*/
 
-/*
-  Calcule u et v tels que au + bv = pgcd, ou pgcd est le pgcd de a et b. 
-  Toutes les variables doivent etre initialisees.
- */
+//Affichge de "a= bq + bv + r"
+void printdivision(mpz_t a, mpz_t b, mpz_t q, mpz_t r) 
+{
+      mpz_out_str(NULL, 10, a);
+      printf(" = ");
+      mpz_out_str(NULL, 10, b);
+      printf(" * ");
+      mpz_out_str(NULL, 10, q);
+      printf(" + ");
+      mpz_out_str(NULL, 10, r);
+      printf("\n");
+}
 
+//Calcul des coefficients de Bezout u e v tels que a*u + b*v = pgcd de a et b
 void bezout(mpz_t a, mpz_t b, mpz_t u, mpz_t v, mpz_t pgcd, mpz_t lastV)
 {
   if (!mpz_cmp_ui(b, 0))
@@ -117,53 +107,50 @@ void bezout(mpz_t a, mpz_t b, mpz_t u, mpz_t v, mpz_t pgcd, mpz_t lastV)
     }
 }
 
-/*---------------------------------------------------------------------*/
-
-
-
-/*---------------------------------------------------------------------*/
-
+//Fonction euclide
+//Recuperation des coefficients u et v (a partir de a et p) 
+//et affectation dans uFinal et vFinal (variables globales)
 int euclide(mpz_t a, mpz_t ptmp)
 {
-  
+ 
+  //Initialisation des variables de type mpz_t
   mpz_t zero,x,y,u,v,inv_a;
-
   mpz_init(inv_a);
-
   mpz_init(zero);
   mpz_init(x);
   mpz_init(y);
   mpz_init(u);
   mpz_init(v);
-  
+  //Affectation des valeurs aux variables correcpondantes
   mpz_init_set_ui(zero, 0);
   mpz_set(x, a);
   mpz_set(y, ptmp);
-  //mpz_init_set_ui(x, mpz_get_ui(a));
-  //mpz_init_set_ui(y, mpz_get_ui(p));
 
+  //Application de la fonction bezout(...) pour recuperer u et v
   bezout(x, y, u, v, ptmp, zero);
 
+  //Initialisation de l inverse modulaire de a
   mpz_init_set_ui(inv_a, 0);
-  
-//*******
+  //Initialisation de uFinal et vFinal
   mpz_init(uFinal);
   mpz_init(vFinal);
 
-  //Affectation des derniers valeur de u et v
+  //Affectation des dernieres valeurs de u et v à uFinal et vFinal
   mpz_set(uFinal, u);
   mpz_set(vFinal, v);
 
+  //Calcul de l inverse modulaire de a
+  //affectation de u mod p_global a inv_a
   mpz_mod(inv_a,u,p_global);
 
+  //Affichage de l'inverse modulaire de a
   printf("\n\n\n inv_a : ");
   mpz_out_str(NULL, 10,inv_a);
 
-  mpz_clear(u);
+  //Nettoyage des variables mpz_t
   mpz_clear(v);
   mpz_clear(x);
   mpz_clear(y);
-
   mpz_clear(inv_a);
 
   return 0;
@@ -172,7 +159,7 @@ int euclide(mpz_t a, mpz_t ptmp)
 int main(int argc, char const *argv[])
 {
 
-  //Initialisation des variables mpz
+  //Initialisation des variables de type mpz_t
   mpz_t a;
   mpz_t p;
   mpz_inits(a,p,p_global,NULL);
@@ -182,8 +169,6 @@ int main(int argc, char const *argv[])
   mpz_init_set_ui(p_global, 60);
   euclide(a,p);
 
-  //mpz_t uFinal;
-  //mpz_t vFinal;
 
   printf("\n\n\n*****lalalalalalalalalalalalalalala*****\n");
   printf("\n\n\nUFINAL : ");
