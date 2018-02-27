709ec5d987e1ea8c9529c27aa676bf210d50b9f9
samybtn <samybentounes@gmail.com>
2018-02-27 18:20:48 +0100

    [ADD] encrypt() fonction

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ mpz_t vFinal;
 //Variable reprensentant p
 mpz_t p_global;
 
+mpz_t grand_B, grand_C;
 
 /*
  * Calcule de A ≡ g^a mod p de la question 4
@@ -74,28 +75,53 @@ void expMod(mpz_t res, mpz_t g, mpz_t a, mpz_t p){
  *X ≡ g x mod p. Elle renvoie en sortie x et X.
 */
 void keyGen(mpz_t p, mpz_t g, mpz_t grand_X, mpz_t x){
-
     gmp_randstate_t state;
-
     // init random state
     gmp_randinit_mt(state);
     gmp_randseed_ui(state, time(NULL));
-
     //gmp_randseed_ui(state, 123987);
-    
     //Generation de x la cle privée 
     mpz_urandomm(x, state, p);
     //mpz_add_ui(x, x, 2);
-
     // calcule du grand X ≡ g^x mod p --> 1eme methode
     expMod(grand_X, g, x, p);
     gmp_printf("===publicKeyOfA grand_X ===\n%Zd\n", grand_X);
-
     // calcule du grand X ≡ g^x mod p --> 2eme methode
     // mpz_powm(grand_X, g, x, p);   
     // gmp_printf("===publicKeyOfA grand_X ===\n%Zd\n", grand_X);
 }
 
+/**
+ * Encrypt() est la fonction qui produit en sortie le couple chiffré (C ≡ m × y mod p, B ≡
+ * g^r mod p) en prenant en entrée la clé publique de Bob K p = (p, g, X) et un message m
+ * avec y ≡ X^r mod p et r alea.
+ */
+void encrypt(mpz_t p, mpz_t g, mpz_t grand_X, mpz_t message) {
+	//Initialisation des variables
+	mpz_t y, r;
+	mpz_inits(r, y, NULL);
+	
+	//r aleatoire
+	gmp_randstate_t state;
+    // init random state
+    gmp_randinit_mt(state);
+    gmp_randseed_ui(state, time(NULL));
+    //generation de r aleatoire 
+    mpz_urandomm(r, state, p);
+    //gmp_printf("r encrypt : %Zd", r);
+    
+    //------------FAIRE UNE LISTE POUR DIFFERENCIER CHAQUE R---------//
+
+	//y (y = X^r mod p)
+	expMod(y, grand_X, r, p);
+	//gmp_printf("y : \n%Zd", y);
+
+	//grand_B (B = g^r mod p)
+	expMod(grand_B, g, r, p);
+	gmp_printf("B : \n%Zd", grand_B);
+	
+
+}
 
 /*
     main
@@ -105,7 +131,7 @@ int main(int argc, char * argv[]){
     mpz_t g,p;
     mpz_t x,grand_X,y1;
 
-    mpz_inits(x,grand_X,y1,NULL);
+    mpz_inits(x,grand_X, grand_B, grand_C, y1,NULL);
 
     // G initialization
     mpz_init(g);
@@ -119,6 +145,9 @@ int main(int argc, char * argv[]){
     
     //avoir Kp=(p,g,X), Ks=(x) 
     keyGen(p,g,grand_X,x);
+    mpz_t mes;
+    mpz_init(mes);
+    encrypt(p, g, grand_X, mes);
   
 
 
